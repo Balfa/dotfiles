459cc7c1112be65f0fe7f6e915683fe34507dc4b
Mo <Balfa@users.noreply.github.com>
2024-02-13 21:19:47 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# install.sh - installs the dotfiles from this repository
+# usage: curl -sL https://raw.githubusercontent.com/Balfa/dotfiles/main/install.sh | cmd
+# currently included in `post-install-first-steps.md`
+
+cd ~
+wget https://raw.githubusercontent.com/Balfa/dotfiles/main/.bash_profile
+wget https://raw.githubusercontent.com/Balfa/dotfiles/main/.bashrc
+wget https://raw.githubusercontent.com/Balfa/dotfiles/main/.gitconfig
