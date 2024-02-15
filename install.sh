42ecf62dc9e060343f2b32e7afd39c6b43f12dbb
Mo <Balfa@users.noreply.github.com>
2024-02-14 23:23:42 -0600

    add vs code files to install. also --timestamping.

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 # currently included in `post-install-first-steps.md`
 
 cd ~
-wget https://raw.githubusercontent.com/Balfa/dotfiles/main/.bash_profile
-wget https://raw.githubusercontent.com/Balfa/dotfiles/main/.bashrc
-wget https://raw.githubusercontent.com/Balfa/dotfiles/main/.gitconfig
+wget --timestamping https://raw.githubusercontent.com/Balfa/dotfiles/main/.bash_profile
+wget --timestamping https://raw.githubusercontent.com/Balfa/dotfiles/main/.bashrc
+wget --timestamping https://raw.githubusercontent.com/Balfa/dotfiles/main/.gitconfig
+wget --timestamping https://raw.githubusercontent.com/Balfa/dotfiles/main/.gitconfig
+mkdir -p AppData/Roaming/Code/User/
+cd AppData/Roaming/Code/User/
+wget --timestamping https://raw.githubusercontent.com/Balfa/dotfiles/main/keybindings.json
+wget --timestamping https://raw.githubusercontent.com/Balfa/dotfiles/main/settings.json
